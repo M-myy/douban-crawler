7390cd38f4c19d6f7cb15efb54731f483a2daa46
M-myy <49876053+M-myy@users.noreply.github.com>
2022-11-19 21:02:14 +0800

    Update crawler.py
    修改

--- crawler.py
+++ crawler.py
@@ -13,7 +13,7 @@ headers = {
 def load_page_url(page_url):
     response = requests.get(page_url,headers = headers)
     response_all_page = bs4.BeautifulSoup(response.text,"lxml")  #提取这个到页面
-    response_paginator = response_all_page.find("div",attrs={'class':'paginator'})  #查找div里面 paginator 类的标签便于查找这个帖子所有的分页
+    response_paginator = response_all_page.find("div",attrs={'class':'paginator'})
 
     all_page_url = set()
     all_page_url.add(page_url)
@@ -28,13 +28,13 @@ def download_email_addr(url_set,file_path):
     for tmp_url in url_set:
         print(f'''正在保存分页:{tmp_url}...''')
         response = requests.get(tmp_url,headers = headers)
-        response_all_page = bs4.BeautifulSoup(response.text,"lxml")  #提取这个到页面
-        response_reply_doc = response_all_page.find_all("div",attrs={'class':'reply-doc'})  #查找div里面 reply-doc 类的标签
+        response_all_page = bs4.BeautifulSoup(response.text,"lxml")
+        response_reply_doc = response_all_page.find_all("div",attrs={'class':'reply-doc'})
 
-        for response_ele in response_reply_doc:  #便利reply-doc类标签
-            response_reply_content = response_ele.find("p",attrs={'class':"reply-content"})  #查找 reply-doc 类标签里面的 reply-content 类p标签
+        for response_ele in response_reply_doc:
+            response_reply_content = response_ele.find("p",attrs={'class':"reply-content"}) 
             reply_email_addr = re.search("\w+@\w+.\w+",response_reply_content.text,flags=re.A)  #正则匹配邮箱
-            #reply_email_addr = re.search("^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$",response_reply_content.text,flags=re.A)  #正则匹配邮箱另一种表达式
+            #reply_email_addr = re.search("^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$",response_reply_content.text,flags=re.A)
             if reply_email_addr:  #如果匹配到邮箱则保存
                 #print(reply_email_addr.group())
                 file_eamil.write(reply_email_addr.group() + '\n')
